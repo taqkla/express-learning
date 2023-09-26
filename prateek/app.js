0f98324752199978ed9c883f39a37b2a7f413bea
Prateek Shukla <shuklaprateek@google.com>
2023-09-26 22:16:35 +0530

    assignment info

---
+++ prateek/app.js
@@ -0,0 +1,16 @@
+// create simple express server
+
+
+// use middlewares
+// validation
+// jwt authentication -- write/update/delete
+// inbuilt validation
+// logger
+
+
+// 4 APIs
+// Get User
+// Get all Users
+// Post user
+// Update a user - can't update a userId
+// Delete a particular user
---
+++ prateek/model/data.js
@@ -0,0 +1,12 @@
+// export your data and process it if needed
+// read write back
+
+function read(readType = "user/post") {
+    
+}
+
+function write(readType = "user/post", data) {
+
+}
+
+module.export = {read, write};
